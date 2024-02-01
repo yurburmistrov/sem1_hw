249c6e0f45329cf0e192dd59f402c60c5f67bb35
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-02 00:23:11 +0300

    Выполнил третье задание

---
+++ hw_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите целое число из отрезка [10, 99]");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшей цифрой числа {number} является {firstDigit}");
+}
+
+if (firstDigit < secondDigit)
+{
+    Console.WriteLine($"Наибольшей цифрой числа {number} является {secondDigit}");
+}
+
+if (firstDigit == secondDigit)
+{
+    Console.WriteLine($"Число {number} является палиндромным!");
+}
