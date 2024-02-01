8bc5d4da8fdbcd794e730e062c6bedde67de071f
Yuriy Burmistrov <yurburmistrov@gmail.com>
2024-02-01 23:52:11 +0300

    Выполнил второе задание

--- hw_2/obj/Debug/net8.0/hw_2.AssemblyInfo.cs
+++ hw_2/obj/Debug/net8.0/hw_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3664b81860b9ce207fe3472c45ea405026c38d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c143222687020089cd5c2b4dbc7c9c4f1a6ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
